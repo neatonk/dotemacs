1851ff59faa57a014cef7eb8ec5b1ae39088cf6f
Kevin Neaton <neatonk@gmail.com>
2012-07-23 11:04:03 -0500

    setup haskell-mode

--- init.el
+++ init.el
@@ -44,3 +44,16 @@
 (require 'paredit)
 (defun turn-on-paredit () (paredit-mode 1))
 (add-hook 'clojure-mode-hook 'turn-on-paredit)
+
+;;;;;;;;;;;;;;;;;;
+;; Haskell Mode ;;
+;;;;;;;;;;;;;;;;;;
+
+(load "~/.emacs.d/lib/haskell-mode/haskell-site-file.el")
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+
+(add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
