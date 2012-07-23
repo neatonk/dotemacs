28f793154e80e4d24d7dd969b4dd7788d42262d6
Kevin Neaton <neatonk@gmail.com>
2012-07-23 11:03:51 -0500

    setup clojure-mode

--- init.el
+++ init.el
@@ -24,3 +24,23 @@
 (setq auto-mode-alist (append '(("\\.nix$" . nix-mode)) auto-mode-alist))
 (autoload 'nix-mode "nix-mode" "nix mode" t)
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Clojure slime/swank-clojure ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Load and configure SLIME
+(add-to-list 'load-path "lib/slime")
+(require 'slime)
+(eval-after-load 'slime '(setq slime-protocol-version 'ignore))
+(slime-setup '(slime-repl))
+
+;; Load a major mode for editing Clojure code.
+(add-to-list 'load-path "lib/clojure-mode")
+(require 'clojure-mode)
+(require 'clojure-test-mode) ;; requires slime
+
+;; Add paredit hook
+(require 'paredit)
+(defun turn-on-paredit () (paredit-mode 1))
+(add-hook 'clojure-mode-hook 'turn-on-paredit)
