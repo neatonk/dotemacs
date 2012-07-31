4bc85fc6009c245d0a4a6c2e404c75bc5360a279
Kevin Neaton <neatonk@gmail.com>
2012-07-30 19:09:21 -0500

    Use root-path help fn.

--- config/user/init.el
+++ config/user/init.el
@@ -31,10 +31,8 @@
 
 ;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
 (custom-set-variables
-  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
-  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+  '(auto-save-file-name-transforms '((".*" (root-path "autosaves/\\1") t)))
+  '(backup-directory-alist '((".*" . (root-path "backups/")))))
 
 ;; create the autosave dir if necessary, since emacs won't.
 (make-directory "~/.emacs.d/autosaves/" t)
-
-
