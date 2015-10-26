76f946c82c46d8677ef61cd7a3d3a9c1cd3a3b58
Kevin Neaton <neatonk@gmail.com>
2015-10-26 11:49:56 -0400

    Use tabs in tt-mode

--- user/el-get/init-tt-mode.el
+++ user/el-get/init-tt-mode.el
@@ -4,3 +4,5 @@
 (autoload 'tt-mode "tt-mode")
 (add-to-list 'auto-mode-alist '("\\.tt\\'" . tt-mode))
 (add-to-list 'auto-mode-alist '("\\.tt2\\'" . tt-mode))
+(add-to-list 'tt-mode-hook
+             (lambda () (set-variable 'indent-tabs-mode t)))
