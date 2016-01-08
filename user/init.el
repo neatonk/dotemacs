d74659c182b610f23a3706edceefe71c7e0b25b9
Kevin Neaton <neatonk@gmail.com>
2016-01-08 10:45:05 -0600

    Avoid error when opam has not been initialized

--- user/init.el
+++ user/init.el
@@ -73,8 +73,9 @@
 
 ;; Load Path
 ;; - Add opam emacs directory
-(let ((opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1)))
-  (add-to-list 'load-path (concat opam-share "/emacs/site-lisp")))
+(let ((opam-share (shell-command-to-string "opam config var share 2> /dev/null")))
+  (when (not (equal opam-share ""))
+    (add-to-list 'load-path (concat (substring opam-share 0 -1) "/emacs/site-lisp"))))
 
 ;; Autoloads
 (autoload 'utop "utop" "Toplevel for OCaml" t)
