1de9e1c3e614df855a92915b6ce10da9960cdf78
Kevin Neaton <kevin@otherpeoplespixels.com>
2019-03-07 13:54:45 -0500

    Added unfill-* functions

--- user/functions.el
+++ user/functions.el
@@ -20,3 +20,24 @@
   (interactive)
   (pbcopy)
   (delete-region (region-beginning) (region-end)))
+
+(defun unfill-region (beg end)
+  "Unfill the region, joining text paragraphs into a single logical line."
+  (interactive "*r")
+  (let ((fill-column (point-max)))
+    (fill-region beg end)))
+
+;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+
+;; Handy key definition
+;;(define-key global-map "\C-\M-Q" 'unfill-region)
+
+;; Handy key definition
+;;(define-key global-map "\M-Q" 'unfill-paragraph)
