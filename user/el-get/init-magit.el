ffb29dad50d2006988256e937f7e11d5a3c9cc75
Kevin Neaton <kevin@otherpeoplespixels.com>
2019-12-02 12:30:24 -0500

    Try to use existing magit-status-mode buffer

--- user/el-get/init-magit.el
+++ user/el-get/init-magit.el
@@ -2,6 +2,13 @@
 ;; config/magit-conf.el
 
 ;;(require 'magit)
-(autoload 'magit-status "magit" nil t)
-(global-set-key (kbd "C-x m") 'magit-status)
 
+(defun my-magit-status ()
+  (interactive)
+  (if-let ((buf (magit-get-mode-buffer 'magit-status-mode)))
+      (progn (switch-to-buffer buf)
+             (magit-refresh-buffer))
+    (magit-status)))
+
+(autoload 'magit-status "magit" nil t)
+(global-set-key (kbd "C-x m") 'my-magit-status)
