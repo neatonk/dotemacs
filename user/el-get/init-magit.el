5a9310261c141dec5286a1cd6db3dc1a8aca9d4b
Kevin Neaton <kevin@otherpeoplespixels.com>
2019-12-18 10:02:06 -0500

    Require magit before calling magit-get-mode-buffer fn

--- user/el-get/init-magit.el
+++ user/el-get/init-magit.el
@@ -1,14 +1,12 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; config/magit-conf.el
 
-;;(require 'magit)
-
 (defun my-magit-status ()
   (interactive)
+  (require 'magit)
   (if-let ((buf (magit-get-mode-buffer 'magit-status-mode)))
       (progn (switch-to-buffer buf)
              (magit-refresh-buffer))
     (magit-status)))
 
-(autoload 'magit-status "magit" nil t)
 (global-set-key (kbd "C-x m") 'my-magit-status)
